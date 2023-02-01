60bdc12f3bb6fc2afb8dc88d5ec2bbbb96e8d698
Dominik Piskorz <dominik.piskorz@scanye.pl>
2023-02-01 12:42:13 +0000

    Log db errors

--- api/src/main/scala/api/DatabaseTransaction.scala
+++ api/src/main/scala/api/DatabaseTransaction.scala
@@ -15,6 +15,17 @@ object DatabaseTransaction {
       }
   }
 
+  implicit class errorExt[A](op: ZIO[Database, ApiError, A]) {
+    def logDbError(): ZIO[Database, ApiError, A] =
+      op.flatMapError {
+          case dbError: ApiError.Database =>
+            for {
+              _ <- ZIO.log(dbError.getMessage)
+            } yield dbError
+          case e => ZIO.succeed(e)
+      }
+  }
+
   def dbTransaction[T](action: String)(op: ZIO[Connection, ApiError, T]): ZIO[Database, ApiError, T] =
-      Database.transaction(op).mapDbError(action).logError("Error during db transaction")
+      Database.transaction(op).mapDbError(action).logDbError()
 }
